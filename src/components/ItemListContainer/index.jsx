d958417599097038f84ffa311b40a7a7a05c9e49
Eugenia Grima <eugeniagrima@gmail.com>
2022-08-09 16:45:54 -0300

    desafios

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -4,8 +4,8 @@ import React, {useState, useEffect} from "react";
 import Title from "../Title";
 
 const comidas = [
-  { id:1, image: "https://pixabay.com/images/id-1971552/", title: "dulce"},
-  { id:2, image: "https://pixabay.com/images/id-1799731/", title:"salado"},
+  { id:1, image: "https://pixabay.com/images/id-1971552/", title: "torta"},
+  { id:2, image: "https://pixabay.com/images/id-1799731/", title:"pan"},
 ];
 
 export const ItemListContainer = ({texto}) => {
