9a88d9c4fee5faeb538c4141789191351a88a0c8
Eugenia Grima <eugeniagrima@gmail.com>
2022-08-09 14:54:13 -0300

    desafios

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,9 +1,19 @@
+import ItemCount from "../ItemCount";
 import React from "react";
 import Title from "../Title";
 
+
 export const ItemListContainer = ({texto}) => {
+    
+    const onAdd= (quantity) => {
+        console.log("compraste " + {quantity} + " unidades");
+    }
+    
     return (
+       <>
         <Title bienvenida={texto}/>
+        <ItemCount initial={1} stock={10} onAdd={onAdd}/>
+       </>
     );
 }
 
