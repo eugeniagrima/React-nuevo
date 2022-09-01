c25e25fd01a0b85e0b5bc4e6fa14d2139b54216b
Eugenia Grima <eugeniagrima@gmail.com>
2022-09-01 17:40:54 -0300

    desafio 1-9

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,3 +1,4 @@
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import React from "react";
 import { useCartContext } from "../../context/CartContext";
 import {Link} from "react-router-dom";
@@ -5,6 +6,24 @@ import ItemCart from "../ItemCart";
 
 const Cart = () => {
     const {cart, totalPrice} = useCartContext();
+
+    const order = {
+        buyer: {
+            name: "Eugenia",
+            email: "eugeniagrima@gmail.com",
+            phone: "1153296298",
+            address: "Salguero 3048"
+        },
+        items: cart.map(product => ({ id: product.id, title: product.title, price: product.price, quantity: product.quantity})),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestore();
+        const ordersCollection = collection(db, "orders");
+        addDoc(ordersCollection, order)
+        .then(({id}) => console.log (id))
+    }
 
     if (cart.lenght === 0) {
         return (
@@ -23,6 +42,7 @@ const Cart = () => {
         <p>
             Total: {totalPrice()}
         </p>
+        <button onClick={handleClick}>Ir a pagar!</button>
         </>
     )
    }
